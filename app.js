6bd8e4d3e6c8ddb98c86ed3b0d422d10ad1c2de1
Matias Silvera <matusilvera808@gmail.com>
2025-06-27 11:02:35 -0300

    Se configuro el server principal

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import booksRouter from "./src/routes/book.routes.js";
+import { startDB } from "./src/config/database.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use("/api", booksRouter);
+
+startDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Servidor escuchando en el puerto ${PORT}`);
+    })
+}).catch(err=>{
+    console.log(err);
+});
