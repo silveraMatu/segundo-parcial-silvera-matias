8b6eb3bb34b615bf96ae4f4de85211d3f3a7f05a
Matias Silvera <matusilvera808@gmail.com>
2025-06-27 12:02:16 -0300

    Se ha cambiado el metodo patch por el put

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 router.post("/books", createBook);
 router.get("/books", getAllBooks);
 router.get("/books/:id", getBooksByID);
-router.patch("/books/:id", updateBooks);
+router.put("/books/:id", updateBooks);
 router.delete("/books/:id", deleteBooks);
 
 export default router;
